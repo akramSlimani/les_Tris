bfaf359e6ae1d940f13a09db843ea96c43b7e9dd
akramSlimani <99551660+akramSlimani@users.noreply.github.com>
2023-05-28 19:30:01 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,9 +1,8 @@
-package TP1;
-
 public class Test {
 
 	public static void main(String[] args) {
 
+		  //donner une taille au tableau
 		  int n = 5; 
 		  TableauEl t1 = new TableauEl(n);
 
@@ -13,12 +12,12 @@ public class Test {
 		   System.out.println("le tableau généré : \n" + t1);
 		  
 	
-
 		       
 		          long debut = System.currentTimeMillis();
-		        // t1.triBulle();
-		        t1.triDenombrement(); 	
-		       //   t1.triRapide(); 
+		        //choisir une méthode de tris :
+		        t1.triBulle();
+		        //t1.triDenombrement(); 	
+		        //t1.triRapide(); 
 		         
 		          long fin = System.currentTimeMillis();
 		          
